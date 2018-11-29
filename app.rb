3fe85f3865f244e1d66f12c7499f682887ef4e33
Vlad Mescherkin <vmescherkin@gmail.com>
2018-11-29 20:02:53 +0100

    Finish working palindrome detector

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require 'sinatra'
+require 'vmescherkin_palindrome'
 
 get '/' do
+  @title = 'Home'
   erb :index
 end
 
 get '/about' do
+  @title = 'About'
   erb :about
 end
 
 get '/palindrome' do
+  @title = 'Palindrome Detector'
   erb :palindrome
+end
+
+post '/check' do
+  @phrase = params[:phrase]
+  erb :result
 end
